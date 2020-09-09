213abd24ac167a26d95f29b29fb0cc403491be3e
andbret <and.bretnall@gmail.com>
2020-09-09 17:04:40 -0400

    complete

--- quiz.js
+++ quiz.js
@@ -1,3 +1,4 @@
+// sets the starting condition for several elements
 document.getElementById("begin").hidden = false;
 document.getElementById("begin2").hidden = false;
 document.getElementById("begin3").hidden = false;
@@ -9,19 +10,13 @@ document.getElementById("buttonA").disabled = false;
 document.getElementById("buttonB").disabled = false;
 document.getElementById("buttonC").disabled = false;
 document.getElementById("buttonD").disabled = false;
-document.getElementById("formmmm").hidden = true;
+document.getElementById("scoreForm").hidden = true;
 
-document.getElementById("idk").hidden = true;
-// var count = 60;
+document.getElementById("bug").hidden = true;
 var correct = 0;
 var score = 0;
-
-// var question1 = '{"title":"Inside which HTML element do we put the JavaScript?","a":"&lt;javascript&gt;","b":"&lt;scripting&gt;", "c":"&lt;script&gt;", "d":"&lt;js&gt;", "answer":"&lt;script&gt;"}'
-// var question2 ='{"title":"How do you write &quot;Hello World&quot; in an alert box?","a":"alertBox(&quot;Hello World&quot;);","b":"msg(&quot;Hello World&quot;);", "c":"alert(&quot;Hello World&quot;);", "d":"msgBox(&quot;Hello World&quot;);", "answer":"alert(&quot;Hello World&quot;);"}'
-// var question3 = '{"title":"How to write an IF statement in JavaScript?","a":"if (i == 5)","b":"if i = 5 then", "c":"if i = 5", "d":"if i == 5 then", "answer":"if (i == 5)"}'
-// var question4 = '{"title":"How to write an IF statement for executing some code if &quot;i&quot; is NOT equal to 5?","a":"if i &lt;&gt; 5","b":"if i =! 5 then", "c":"if (i != 5)", "d":"if (i &lt;&gt; 5)", "answer":"if (i != 5)"}'
+// all quiz questions
 var question1 = '{"title":"Which operator is used to assign a value to a variable?", "a":"x", "b":"-", "c":"=", "d":"*", "answer":"="}'
-
 var question2 = '{"title":"Which event occurs when the user clicks on an HTML element?", "a":"onchange ", "b":"onclick", "c":"onmouseclick", "d":"onmouseover", "answer":"onclick"}'
 var question3 = '{"title":"How do you find the number with the highest value of x and y?", "a":"top(x, y) ", "b":"Math.ceil(x, y)", "c":"Math.max(x, y)", "d":"ceil(x, y)", "answer":"Math.max(x, y)"}'
 var question4 = '{"title":"How do you round the number 7.25, to the nearest integer?", "a":"Math.round(7.25) ", "b":"rnd(7.25)", "c":"Math.rnd(7.25)", "d":"round(7.25)", "answer":"Math.round(7.25) "}'
@@ -31,42 +26,39 @@ var question7 = '{"title":"How do you write &quot;Hello World&quot; in an alert 
 var question8 = '{"title":"How do you declare a JavaScript variable?", "a":"carName","b":"v carName;", "c":"variable carName;", "d":"var carName;", "answer":"var carName;"}'
 var question9 = '{"title":"How can you detect the clients browser name?", "a":"browser.name","b":"client.navName", "c":"navigator.appName", "d":"browser.client", "answer":"navigator.appName"}'
 var question10 = '{"title":"How do you create a function in JavaScript?", "a":"function myFunction()","b":"function = myFunction()", "c":"function:myFunction()", "d":"create.function", "answer":"function:myFunction()"}'
-
+// creates an array of the questions
 var questions = [question1, question2, question3, question4, question5, question6, question7, question8, question9, question10];
-
+// displays the correct information depending on the current question
 function renderQuestion() {
     var scoreEl = document.querySelector("#score");
     scoreEl.textContent = score;
     console.log(correct)
-    // event.preventDefault()
     var i = correct;
 
     var options = JSON.parse(questions[i]);
     var g = options.answer;
-    document.getElementById("demo").innerHTML = options.title;
+    // loads correct content
+    document.getElementById("questionTitle").innerHTML = options.title;
     document.getElementById("buttonA").innerHTML = options.a;
     document.getElementById("buttonB").innerHTML = options.b;
     document.getElementById("buttonC").innerHTML = options.c;
     document.getElementById("buttonD").innerHTML = options.d;
-    // document.getElementById("theAnswer") = options.answer;
 
-
+//    un-hides the buttons
     document.getElementById("buttonA").hidden = false;
     document.getElementById("buttonB").hidden = false;
     document.getElementById("buttonC").hidden = false;
     document.getElementById("buttonD").hidden = false;
-
-    var allBtns = document.querySelectorAll(".btn");
-    console.log(options.answer);
-
 }
+//  lets me select from all buttons
 var allBtns = document.querySelectorAll(".btn");
 
 for (var y = 0; y < allBtns.length; y++) {
 
     allBtns[y].addEventListener("click", function (event) {
+        // selects a quiestion based on how many the user has correctly answered
         var options = JSON.parse(questions[correct]);
-        // event.preventDefault();
+    //    hides the landing screen content
         document.getElementById("begin").hidden = true;
         document.getElementById("begin2").hidden = true;
         document.getElementById("begin2").hidden = true;
@@ -74,27 +66,30 @@ for (var y = 0; y < allBtns.length; y++) {
         console.log(event.target);
         console.log(event.target.id);
 
+        // checks if the button the user clicked is the correct answer
         if ((event.target.id === "buttonA") && ((options.a) === (options.answer))) {
-
-
-
+// in the event of a correct answer...
             console.log("WINNER!!");
+            // raises question number
             correct++;
             console.log(correct);
-            // count = count + 6;
+// raises the score
             score++;
+            // resets any disabled buttons
             document.getElementById("buttonA").disabled = false;
             document.getElementById("buttonB").disabled = false;
             document.getElementById("buttonC").disabled = false;
             document.getElementById("buttonD").disabled = false;
-
+// starts the next question
             renderQuestion()
         }
         else {
             if (event.target.id === "buttonA") {
+                // reduces the score for an incorrect answer
             count = count - 10;
+            // disables the incorrect option
             document.getElementById("buttonA").disabled = true;
-
+// ensures button content remains the same
             renderQuestion()
             }
         }
@@ -105,7 +100,7 @@ for (var y = 0; y < allBtns.length; y++) {
             console.log("WINNER!!");
             correct++;
             console.log(correct);
-            // count = count + 6;
+
             score++;
             document.getElementById("buttonA").disabled = false;
             document.getElementById("buttonB").disabled = false;
@@ -129,7 +124,6 @@ for (var y = 0; y < allBtns.length; y++) {
             console.log("WINNER!!");
             correct++;
             console.log(correct);
-            // count = count + 6;
             score++;
             document.getElementById("buttonA").disabled = false;
             document.getElementById("buttonB").disabled = false;
@@ -152,15 +146,11 @@ for (var y = 0; y < allBtns.length; y++) {
 
             console.log("WINNER!!");
             correct++;
-            // count = count + 6;
             score++;
             document.getElementById("buttonA").disabled = false;
             document.getElementById("buttonB").disabled = false;
             document.getElementById("buttonC").disabled = false;
             document.getElementById("buttonD").disabled = false;
-            // console.log(theAnswer);
-
-
 
             renderQuestion()
 
@@ -173,7 +163,7 @@ for (var y = 0; y < allBtns.length; y++) {
             renderQuestion()
             }
         }
-
+// starts the quiz
         if (event.target.id === "begin") {
             document.getElementById("buttonA").disabled = false;
             document.getElementById("buttonB").disabled = false;
@@ -187,7 +177,7 @@ for (var y = 0; y < allBtns.length; y++) {
 
 
 }
-
+// timer code
 var count = 60;
 function prepareRead() {
 
@@ -207,7 +197,7 @@ function prepareRead() {
     }, 1000);
     
 }
-
+// conditions for the score submition screen
 function complete() {
     document.getElementById("begin").hidden = true;
     document.getElementById("begin2").hidden = true;
@@ -216,75 +206,55 @@ function complete() {
     document.getElementById("buttonB").hidden = true;
     document.getElementById("buttonC").hidden = true;
     document.getElementById("buttonD").hidden = true;
-    document.getElementById("demo").innerHTML = "Enter yout initials and save your score!";
-    document.getElementById("formmmm").hidden = false;
+    document.getElementById("questionTitle").innerHTML = "Enter yout initials and save your score!";
+    document.getElementById("scoreForm").hidden = false;
 
 }
 
 
 
-var todoInput = document.querySelector("#todo-text");
-var todoForm = document.querySelector("#todo-form");
-var todoList = document.querySelector("#todo-list");
-var todoCountSpan = document.querySelector("#todo-count");
+var scoreInput = document.querySelector("#score-text");
+var scoreForm = document.querySelector("#score-form");
+var scoreList = document.querySelector("#score-list");
+var scoreCountSpan = document.querySelector("#score-count");
 
-var todos = ["Learn HTML", "Learn CSS", "Learn JavaScript"];
+// shows the high scores
+renderscores();
 
-renderTodos();
+function renderscores() {
+  scores = JSON.parse(localStorage.getItem("scores")) || [];
 
-function renderTodos() {
-  todos = JSON.parse(localStorage.getItem("todos")) || [];
+  // Clear scoreList element and update scoreCountSpan
+  scoreList.innerHTML = "";
+  scoreCountSpan.textContent = scores.length+ score;
 
-  // Clear todoList element and update todoCountSpan
-  todoList.innerHTML = "";
-  todoCountSpan.textContent = todos.length+ score;
-
-  // Render a new li for each todo
-  for (var i = 0; i < todos.length; i++) {
-    var todo = todos[i];
+  // Render a new li for each score
+  for (var i = 0; i < scores.length; i++) {
+    var score = scores[i];
 
     var li = document.createElement("li");
-    li.textContent = todo;
-    todoList.appendChild(li);
+    li.textContent = score;
+    scoreList.appendChild(li);
   }
 }
 
-todoForm.addEventListener("submit", function(event) {
+scoreForm.addEventListener("submit", function(event) {
   event.preventDefault();
 
   console.log("Form was submitted!");
 
-  var newTodoText = todoInput.value;
+  var newscoreText = scoreInput.value;
 
-  console.log(newTodoText);
+  console.log(newscoreText);
 
-  todos.push(newTodoText + " " + score);
+  scores.push(newscoreText + " " + score);
 
-  console.log(todos);
+  console.log(scores);
 
-  localStorage.setItem("todos", JSON.stringify(todos));
+  localStorage.setItem("scores", JSON.stringify(scores));
 
-  todoInput.value = "";
+  scoreInput.value = "";
 
-  renderTodos();
+  renderscores();
 });
 
-function reset(){
-    document.getElementById("begin").hidden = false;
-    document.getElementById("begin2").hidden = false;
-    document.getElementById("begin3").hidden = false;
-    document.getElementById("buttonA").hidden = true;
-    document.getElementById("buttonB").hidden = true;
-    document.getElementById("buttonC").hidden = true;
-    document.getElementById("buttonD").hidden = true;
-    document.getElementById("buttonA").disabled = false;
-    document.getElementById("buttonB").disabled = false;
-    document.getElementById("buttonC").disabled = false;
-    document.getElementById("buttonD").disabled = false;
-    document.getElementById("formmmm").hidden = true;
-    // var count = 60;
-    correct = 0;
-    score = 0;  
-    count = 60;
-    document.getElementById("demo").innerHTML = "";
-}
